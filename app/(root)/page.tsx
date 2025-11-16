a23f85694a767eeab03d96827f16abbc7c59da01
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2025-11-16 12:13:29 -0700

    chore: Adding component shared Logo

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { Navbar } from "./components";
 
 export default function Home() {
   return (
     <div className="min-h-screen w-full bg-linear-to-br from-slate-900 via-purple-900 to-slate-900">
-      <p>home</p>
+      <Navbar />
     </div>
   );
 }

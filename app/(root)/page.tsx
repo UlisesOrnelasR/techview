b34145ad0faee7c078323f21b1c0228892242cbc
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2025-11-16 11:42:42 -0700

    chore: Adding moving main page to root folder

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen w-full bg-linear-to-br from-slate-900 via-purple-900 to-slate-900">
+      <p>home</p>
+    </div>
+  );
+}

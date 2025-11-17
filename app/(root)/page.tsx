aede051c152ffe55fa4ab6e3fef95ce545d041f7
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2025-11-16 21:21:02 -0700

    feat: hero block home

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Navbar } from "./components";
+import { HeroBlock, Navbar } from "./components";
 
 export default function Home() {
   return (
     <div className="min-h-screen w-full bg-linear-to-br from-slate-900 via-purple-900 to-slate-900">
       <Navbar />
+      <HeroBlock />
     </div>
   );
 }

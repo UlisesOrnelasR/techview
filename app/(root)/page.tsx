dda5b7ddb3dcf0d550e6a8269f9f926b0ae3b157
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2025-11-17 20:27:38 -0700

    feat: pricing block home

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,16 @@
 import React from "react";
-import { HeroBlock, HowItWorks, Navbar } from "./components";
+import { HeroBlock, HowItWorks, Navbar, Pricing } from "./components";
 
 export default function Home() {
   return (
     <div className="min-h-screen w-full bg-linear-to-br from-slate-900 via-purple-900 to-slate-900">
       <Navbar />
+
       <HeroBlock />
+
       <HowItWorks />
+
+      <Pricing />
     </div>
   );
 }

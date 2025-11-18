bab6931c5ec7f8a82567b3c1c8ceabd74d28d529
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2025-11-17 19:28:56 -0700

    feat: how it works home

---
+++ app/(root)/components/HowItWorks/HowItWorks.data.ts
@@ -0,0 +1,28 @@
+import { MessageSquare, Brain, BarChart3, Trophy } from "lucide-react";
+
+export const howItWorks = [
+  {
+    icon: MessageSquare,
+    title: "Choose Your Interview Type",
+    description:
+      "Select technical codign, system design, behavioral, or company-specific interview formats.",
+  },
+  {
+    icon: Brain,
+    title: "Practice with AI interviewer",
+    description:
+      "Engage in realistic conversations with our AI that adapts to your experience level and role.",
+  },
+  {
+    icon: BarChart3,
+    title: "Get Instant Feedback",
+    description:
+      "Receive detailed analysis on your answers, coding skills, communication, and areas for improvement.",
+  },
+  {
+    icon: Trophy,
+    title: "Track Your Progress",
+    description:
+      "Monito your improvement over time and identify which skills need more practice before the real interview.",
+  },
+];
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { HeroBlock, Navbar } from "./components";
+import { HeroBlock, HowItWorks, Navbar } from "./components";
 
 export default function Home() {
   return (
     <div className="min-h-screen w-full bg-linear-to-br from-slate-900 via-purple-900 to-slate-900">
       <Navbar />
       <HeroBlock />
+      <HowItWorks />
     </div>
   );
 }

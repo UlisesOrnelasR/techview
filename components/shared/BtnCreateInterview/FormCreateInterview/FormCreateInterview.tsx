859b4b18e2d37a8968544363408b002ef0dc9505
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2025-11-19 21:44:36 -0700

    feat: sidebar dashboard

---
+++ components/shared/BtnCreateInterview/FormCreateInterview/FormCreateInterview.tsx
@@ -0,0 +1,126 @@
+"use client";
+
+import { Mic } from "lucide-react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { useForm } from "react-hook-form";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Input } from "@/components/ui/input";
+
+import { formSchema } from "./FormCreateInterview.form";
+import { levels, rols } from "./FormCreateInterview.data";
+
+export function FormCreateInterview() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      role: "",
+      level: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8 text-black"
+      >
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name of the interview</FormLabel>
+              <FormControl>
+                <Input placeholder="Iterview name" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="role"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Select the role</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger className="w-full">
+                    <SelectValue placeholder="Select a role" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  {rols.map((role, index) => (
+                    <SelectItem key={index} value={role.value}>
+                      <div className="flex items-center gap-2">
+                        <span>{role.icon}</span>
+                        <span>{role.label}</span>
+                      </div>
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="level"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Select the difficulty</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger className="w-full">
+                    <SelectValue placeholder="Select the difficulty" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  {levels.map((level, indexLevel) => (
+                    <SelectItem key={indexLevel} value={level.value}>
+                      <div className="flex items-center gap-2">
+                        <span>{level.label}</span>
+                      </div>
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button
+          type="submit"
+          className="
+        w-full bg-linear-to-r from-purple-600 to-blue-600 font-bold py-3 px-6 rounded-lg hover:from-purple-700 hover:to-blue-700 transition-all duration-300
+        "
+        >
+          Start interview <Mic />
+        </Button>
+      </form>
+    </Form>
+  );
+}
